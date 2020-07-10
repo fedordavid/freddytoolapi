4ec7766a38ca10377d0698806f5b4dc88aaa7989
David Fedor <david.fedor1@ibm.com>
2020-07-10 18:34:44 +0200

    fixed previous commit without untracked files

---
+++ freddytoolapi/Freddy.Application/Commands/Products/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,18 @@
+﻿using Freddy.Application.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freddy.Application.Commands.Products
+{
+    public class DeleteProductCommand : Command
+    {
+        public Guid ProductId { get; set; }
+
+        public DeleteProductCommand(Guid productId)
+        {
+            ProductId = productId;
+        }
+    }
+}
---
+++ freddytoolapi/Freddy.UnitTests/Application/Commands/Products/DeleteProductTests.cs
@@ -0,0 +1,31 @@
+﻿using Freddy.Application.Commands.Products;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Freddy.Application.UnitTests.Application.Commands.Products
+{
+    public class DeleteProductTests
+    {
+        private readonly Mock<IProducts> _productsMock;
+        private readonly DeleteProduct _deleteProduct;
+
+        public DeleteProductTests()
+        {
+            _productsMock = new Mock<IProducts>();
+            _deleteProduct = new DeleteProduct(_productsMock.Object);
+        }
+
+        [Fact]
+        public void Execute_ShouldDeleteProduct()
+        {
+            var productId = new Guid("3b50451a-05d1-4e96-a2d7-7ff1e2cca09f");
+
+            _deleteProduct.Handle(new DeleteProductCommand(productId));
+
+            _productsMock.Verify(cmd => cmd.Delete(productId), Times.Once);
+        } 
+    }
+}

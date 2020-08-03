bfccb57bc04ded6b596f6585a835ad628d94a425
David Fedor <david.fedor1@ibm.com>
2020-08-03 14:11:54 +0200

    changes according to suggestion in code review

--- freddytoolapi/Freddy.IntegrationTests/Utilities/HttpClientExtensions.cs
+++ freddytoolapi/Freddy.IntegrationTests/Utilities/HttpClientExtensions.cs
@@ -8,18 +8,23 @@ namespace Freddy.IntegrationTests.Utilities
 {
     public static class HttpClientExtensions
     {
+        public static TObject Deserialize<TObject>(this string str)
+        {
+            return str.Any() ? JsonSerializer.Deserialize<TObject>(str, Options) : default;
+        }
+
         private static readonly JsonSerializerOptions Options = new JsonSerializerOptions
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase
         };
-        
+
         public static async Task<TObject> GetObjectAsync<TObject>(this HttpClient client, string url)
         {
             var response = await client.GetStringAsync(url);
 
-            return response.Any() ? JsonSerializer.Deserialize<TObject>(response, Options) : default;
+            return response.Deserialize<TObject>();
         }
-        
+
         public static async Task<HttpResponseMessage> PostObjectAsync<TObject>(this HttpClient client, string url, TObject o)
         {
             var payload = JsonSerializer.Serialize(o, Options);
